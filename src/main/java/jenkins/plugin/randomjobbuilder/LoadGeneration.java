be9a6935eb536808eb35066f523f9b1a7d20bd4a
Sam Van Oort <samvanoort@gmail.com>
2017-07-24 14:01:35 -0400

    Add helper/utility methods for LoadGeneration and skeleton of trivial impl

--- src/main/java/jenkins/plugin/randomjobbuilder/LoadGeneration.java
+++ src/main/java/jenkins/plugin/randomjobbuilder/LoadGeneration.java
@@ -1,24 +1,38 @@
 package jenkins.plugin.randomjobbuilder;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
+import hudson.model.Cause;
 import hudson.model.Descriptor;
 import hudson.model.Job;
 import hudson.model.Run;
 import hudson.util.DescribableList;
 import jenkins.model.Jenkins;
+import jenkins.model.ParameterizedJobMixIn;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
 
 import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.regex.Pattern;
 
 /**
  * Configurable load generator that runs jobs
  */
 public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
+
+    final static Random rand = new Random();
 
     @Extension
     public static class DescriptorImpl extends Descriptor<LoadGeneration> {
@@ -53,19 +67,133 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
         IDLE,
         RAMP_UP,
         LOAD, // active at full load
-        SHUT_DOWN
+        RAMP_DOWN
+    }
+
+    public static class LoadGeneratorCause extends Cause {
+        final LoadGenerator generator;
+
+        public LoadGeneratorCause(@Nonnull LoadGenerator generator) {
+            this.generator = generator;
+        }
+
+        public LoadGenerator getGenerator(){return generator;}
+
+        @Override
+        public String getShortDescription() {
+            return "LoadGener   ator: "+generator.getClass().getSimpleName();
+        }
+    }
+
+    static class ParameterizedBuilder extends ParameterizedJobMixIn {
+        Job job;
+
+        ParameterizedBuilder(Job j) {
+            this.job = j;
+        }
+
+        @Override protected Job asJob() {
+            return job;
+        }
+    }
+
+
+    @CheckForNull
+    static void launchJob(@Nonnull LoadGenerator generator, @Nonnull Job job, int quietPeriod) {
+        if (job instanceof ParameterizedJobMixIn.ParameterizedJob) {
+            ParameterizedJobMixIn.ParameterizedJob pj = (ParameterizedJobMixIn.ParameterizedJob)job;
+            new ParameterizedBuilder(job).scheduleBuild(quietPeriod, new LoadGeneratorCause(generator));
+        } else {
+            // Can't schedule me, I know, so sad.
+        }
+    }
+
+    /**
+     * Convenience: filter a list of candidate jobs by a condition
+     * @param filterCondition Predicate to filter jobs by
+     * @return Filtered list of jobs
+     */
+    @Nonnull
+    static List<Job> filterJobsByCondition(@Nonnull Predicate<Job> filterCondition) {
+        ArrayList<Job> output = new ArrayList<Job>();
+        Iterables.addAll(output, Iterables.filter(Jenkins.getActiveInstance().getAllItems(Job.class), filterCondition));
+        return output;
+    }
+
+    /**
+     * Convenience: pick a random job from list to run
+     * @param candidates
+     * @return
+     */
+    @CheckForNull
+    static Job pickRandomJob(@Nonnull List<Job> candidates) {
+        if (candidates.size() == 0) {
+            return null;
+        }
+        return candidates.get(rand.nextInt(candidates.size()));
+    }
+
+    /** Filters candidate jobs by pattern matching on fullName */
+    static class JobNameFilter implements Predicate<Job> {
+        String nameMatchRegex = null;
+        Pattern namePattern = null;
+
+        public JobNameFilter(){
+
+        }
+
+        public JobNameFilter(String nameMatchRegex) {
+            if (!StringUtils.isEmpty(nameMatchRegex)) {
+                this.nameMatchRegex = nameMatchRegex;
+                this.namePattern = Pattern.compile(nameMatchRegex);
+            }
+        }
+
+        /** Sets name pattern with fluent-style API  */
+        JobNameFilter setNamePattern(String filterCondition) {
+            if (!StringUtils.isEmpty(nameMatchRegex)) {
+                this.nameMatchRegex = nameMatchRegex;
+                this.namePattern = Pattern.compile(nameMatchRegex);
+            } else {
+                this.nameMatchRegex = null;
+                this.namePattern = null;
+            }
+            return this;
+        }
+
+        @Override
+        public boolean apply(@Nullable Job input) {
+            if (input == null) {
+                return false;
+            }
+            return namePattern.matcher(input.getFullName()).matches();
+        }
     }
 
     /** Base for all load generators that run jobs */
     public static abstract class LoadGenerator extends AbstractDescribableImpl<LoadGenerator> {
 
-        public abstract List<Run> getAllRunningLoad();
+        protected CurrentTestMode currentTestMode = CurrentTestMode.IDLE;
 
-        public abstract List<Run> getAllQueuedLoad();
+        /** Pool of generated jobs */
+        protected HashSet<Run> generated = new HashSet<Run>();
 
-        public abstract CurrentTestMode getTestMode();
+        public CurrentTestMode getTestMode() {
+            return currentTestMode;
+        }
 
         public abstract List<Job> getCandidateJobs();
+
+        /** Begin running load test and then switch to full load
+         *  @return {@link CurrentTestMode} phase as we transition to starting load test, i.e. LOAD or RAMP_UP
+         */
+        public abstract CurrentTestMode start();
+
+        /**
+         * Start shutting down the load test and then stop it
+         * @return {@link CurrentTestMode} as we transition to shutting load load test, i.e. RAMP_DOWN or IDLE
+         */
+        public abstract CurrentTestMode stop();
 
         /** Descriptors neeed to extend this */
         @Extension
@@ -74,6 +202,62 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
             @Override
             public String getDisplayName() {
                 return "";
+            }
+        }
+    }
+
+
+    /** Simple job runner that immediately starts them and keeps restarting then they finish or die */
+    public static class TrivialLoadGenerator extends LoadGenerator {
+
+        private String jobNameRegex = null;
+
+        private int jobConcurrency = 1;
+
+        @Override
+        public List<Job> getCandidateJobs() {
+            return LoadGeneration.filterJobsByCondition(new JobNameFilter(jobNameRegex));
+        }
+
+        @Override
+        public CurrentTestMode start() {
+            return CurrentTestMode.LOAD;
+        }
+
+        @Override
+        public CurrentTestMode stop() {
+            return CurrentTestMode.IDLE;
+        }
+
+        public String getJobNameRegex() {
+            return jobNameRegex;
+        }
+
+        public void setJobNameRegex(String jobNameRegex) {
+            this.jobNameRegex = jobNameRegex;
+        }
+
+        public int getJobConcurrency() {
+            return jobConcurrency;
+        }
+
+        public void setJobConcurrency(int jobConcurrency) {
+            this.jobConcurrency = jobConcurrency;
+        }
+
+        public TrivialLoadGenerator(@CheckForNull String jobNameRegex, int jobConcurrency) {
+            setJobNameRegex(jobNameRegex);
+            if (jobConcurrency < 0) {  // TODO Jelly form validation to reject this
+                this.jobConcurrency = 1;
+            }
+        }
+
+        @Extension
+        public static class DescriptorImpl extends DescriptorBase {
+
+            @Override
+            public String getDisplayName() {
+                return "Trivial Filtered Job Builder";
             }
         }
     }
@@ -144,23 +328,18 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
         }
 
         @Override
-        public List<Run> getAllRunningLoad() {
-            return null;
-        }
-
-        @Override
-        public List<Run> getAllQueuedLoad() {
-            return null;
-        }
-
-        @Override
-        public CurrentTestMode getTestMode() {
-            return null;
-        }
-
-        @Override
         public List<Job> getCandidateJobs() {
-            return null;
+            return Collections.EMPTY_LIST;
+        }
+
+        @Override
+        public CurrentTestMode start() {
+            return CurrentTestMode.IDLE;
+        }
+
+        @Override
+        public CurrentTestMode stop() {
+            return CurrentTestMode.LOAD;
         }
 
         @Extension

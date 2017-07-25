379df6993a1bb6f0b69cc078362cbec9ff43f888
Sam Van Oort <samvanoort@gmail.com>
2017-07-24 21:01:04 -0400

    Mostly flesh out implementation of LoadGeneration modulo rampup/rampdown and better stop logic

--- src/main/java/jenkins/plugin/randomjobbuilder/LoadGeneration.java
+++ src/main/java/jenkins/plugin/randomjobbuilder/LoadGeneration.java
@@ -5,13 +5,21 @@ import com.google.common.collect.Iterables;
 import hudson.Extension;
 import hudson.model.AbstractDescribableImpl;
 import hudson.model.Cause;
+import hudson.model.CauseAction;
 import hudson.model.Descriptor;
+import hudson.model.Executor;
 import hudson.model.Job;
+import hudson.model.PeriodicWork;
 import hudson.model.Run;
+import hudson.model.listeners.RunListener;
+import hudson.model.queue.QueueTaskFuture;
+import hudson.security.ACL;
 import hudson.util.DescribableList;
 import jenkins.model.Jenkins;
 import jenkins.model.ParameterizedJobMixIn;
 import net.sf.json.JSONObject;
+import org.acegisecurity.context.SecurityContext;
+import org.acegisecurity.context.SecurityContextHolder;
 import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.StaplerRequest;
@@ -21,11 +29,16 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.regex.Pattern;
 
 /**
@@ -119,11 +132,22 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
 
 
     @CheckForNull
-    static void launchJob(@Nonnull LoadGenerator generator, @Nonnull Job job, int quietPeriod) {
+    static QueueTaskFuture<? extends Run> launchJob(@Nonnull LoadGenerator generator, @Nonnull Job job, int quietPeriod) {
         if (job instanceof ParameterizedJobMixIn.ParameterizedJob) {
-            ParameterizedJobMixIn.ParameterizedJob pj = (ParameterizedJobMixIn.ParameterizedJob)job;
-            new ParameterizedBuilder(job).scheduleBuild(quietPeriod, new LoadGeneratorCause(generator));
+            SecurityContext old = null;
+            try {
+                old = ACL.impersonate(ACL.SYSTEM);
+                ParameterizedJobMixIn.ParameterizedJob pj = (ParameterizedJobMixIn.ParameterizedJob)job;
+                QueueTaskFuture<? extends Run> future = new ParameterizedBuilder(job).scheduleBuild2(quietPeriod, new CauseAction(new LoadGeneratorCause(generator)));
+                return future;
+            } finally {
+                if (old != null) {
+                    SecurityContextHolder.setContext(old);
+                }
+            }
+
         } else {
+            return null;
             // Can't schedule me, I know, so sad.
         }
     }
@@ -190,6 +214,159 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
         }
     }
 
+    /** Registers run for lookup, so we know which ones to kill if we stop load suddenly...
+     *   or in which cases we need to start new jobs to maintain load level
+     *
+     *   And triggers new builds when needed
+     *
+     *
+     */
+    @Extension
+    static class GeneratorController extends RunListener<Run> {
+        ConcurrentHashMap<Run, LoadGenerator> mapRunToGenerator = new ConcurrentHashMap<>();
+        ConcurrentHashMap<LoadGenerator, Collection<Run>> generatorToRuns = new ConcurrentHashMap<>();
+
+        public void registerRun(@Nonnull Run run, @Nonnull LoadGenerator generator) {
+            synchronized (generator) {
+                mapRunToGenerator.put(run, generator);
+                Collection<Run> runList = generatorToRuns.get(generator);
+                if (runList != null) {
+                    runList.add(run);
+                } else {
+                    HashSet<Run> set = new HashSet<Run>();
+                    set.add(run);
+                    generatorToRuns.put(generator, set);
+                }
+            }
+        }
+
+        /** Return number of new runs we need to schedule or 0 if none */
+        int scheduleMoreCount(@Nonnull LoadGenerator gen) {
+            if (gen.getTestMode() == CurrentTestMode.IDLE || gen.getTestMode() == CurrentTestMode.RAMP_DOWN) {
+                return 0; // We're trying to shut DOWN load here
+            }
+            int desired = gen.getDesiredRunCount();
+            int existing = generatorToRuns.get(gen).size();
+            return desired-existing;
+        }
+
+        int getRunCount(@Nonnull LoadGenerator gen) {
+            synchronized (gen) {
+                Collection<Run> existing = generatorToRuns.get(gen);
+                return (existing != null) ? existing.size() : 0;
+            }
+        }
+
+        /** Triggers runs as requested by the LoadGenerator */
+        public int triggerRuns(@Nonnull LoadGenerator gen) {
+            int count = getRunCount(gen);
+            int launchCount = gen.getRunsToLaunch(count);
+            synchronized (gen) {
+                List<Job> candidates = gen.getCandidateJobs();
+                for (int i=0; i<launchCount; i++) {
+                    Job j = pickRandomJob(candidates);
+                    QueueTaskFuture<? extends Run> qtf = launchJob(gen, j, 0);
+                    try {
+                        // TODO do we get a run or a Task or what, here????
+                        registerRun(qtf.get(), gen);
+                    } catch (InterruptedException | ExecutionException ex) {
+                        // Nope
+                    }
+                }
+            }
+            return launchCount;
+        }
+
+        // TODO delegate somehow to the LoadGenerator for how it's going to create new runs
+
+
+        public void stopAbruptly(@Nonnull final LoadGenerator gen) {
+            SecurityContext context;
+            synchronized (gen) {
+                ACL.impersonate(ACL.SYSTEM, new Runnable() {
+                    @Override
+                    public void run() {
+                        Collection<Run> runs = generatorToRuns.get(gen);
+                        if (runs == null || runs.isEmpty()) {
+                            return;
+                        }
+
+                        for (Run r : runs) {
+                            // Cancel if in queue
+                            Executor ex = r.getExecutor();
+                            if (ex == null) {
+                                ex = r.getOneOffExecutor();
+                            }
+                            if (ex != null) {
+                                ex.doStop();
+                            } else {
+                                //WorkflowRun.doKill();
+                            }
+                            unregisterRun(r);
+                        }
+                    }
+                });
+            }
+        }
+
+        /** Unregister run, return its generator */
+        @CheckForNull
+        public LoadGenerator unregisterRun(@Nonnull Run run) {
+            LoadGenerator gen =  mapRunToGenerator.remove(run);
+            synchronized (gen) {
+                if (gen != null) {
+                    Collection<Run> running = generatorToRuns.get(gen);
+                    if (running != null && !running.isEmpty()) {
+                        running.remove(run);
+                    }
+                }
+                return gen;
+            }
+        }
+
+        @Override
+        public void onFinalized(Run run) {
+            LoadGenerator gen = unregisterRun(run);
+            if (gen != null) {
+                triggerRuns(gen);
+            }
+        }
+
+        @Override
+        public void onDeleted(Run run) {
+            LoadGenerator gen = unregisterRun(run);
+            if (gen != null) {
+                triggerRuns(gen);
+            }
+        }
+    }
+
+    /** Periodically starts up load again if toggled */
+    @Extension
+    public static class PeriodicWorkImpl extends PeriodicWork {
+        GeneratorController controller = Jenkins.getActiveInstance().getExtensionList(GeneratorController.class).get(0);
+
+        @Override
+        public long getRecurrencePeriod() {
+            return 3000L;
+        }
+
+        @Override
+        protected void doRun() throws Exception {
+            LoadGeneration.DescriptorImpl impl = Jenkins.getActiveInstance().getExtensionList(LoadGeneration.DescriptorImpl.class).get(0);
+            if (impl != null) {
+                List<LoadGenerator> loadGenerators = impl.loadGenerators;
+                for (LoadGenerator lg : loadGenerators) {
+                    if (lg.isActive()) {
+                        controller.triggerRuns(lg);
+                    }
+                }
+            } else {
+                throw new RuntimeException("Something wicked this descriptor causes");
+            }
+        }
+    }
+
     /** Base for all load generators that run jobs */
     public static abstract class LoadGenerator extends AbstractDescribableImpl<LoadGenerator> {
         /** Identifies the generator for causes */
@@ -208,6 +385,20 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
             return generatorId;
         }
 
+        public boolean isActive() {
+            return getTestMode() == CurrentTestMode.RAMP_UP || getTestMode() == CurrentTestMode.LOAD_TEST;
+        }
+
+        /** Given current number of runs, launch more if needed.  Return number to fire now, or less than 0 for none
+         *  This allows for ramp-up behavior.
+         */
+        public int getRunsToLaunch(int currentRuns) {
+            if (isActive()) {
+                return getDesiredRunCount()-currentRuns;
+            }
+            return 0;
+        }
+
         public abstract List<Job> getCandidateJobs();
 
         /** Begin running load test and then switch to full load
@@ -221,6 +412,14 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
          */
         public abstract CurrentTestMode stop();
 
+        // TODO some sort of API for how to shut down jobs and how fast?
+
+        /**
+         * Get the intended number of concurrent runs at once
+         * @return -1 for unlimited, 0 if none, or positive integer for intended count
+         */
+        public abstract int getDesiredRunCount();
+
         /** Descriptors neeed to extend this */
         @Extension
         public static class DescriptorBase extends Descriptor<LoadGenerator> {
@@ -230,6 +429,21 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
                 return "";
             }
         }
+
+        @Override
+        public int hashCode() {
+            return getGeneratorId().hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (o == this) {
+                return true;
+            } else if (o == null || !(o instanceof LoadGenerator)) {
+                return false;
+            }
+            return this.getGeneratorId().equals(((LoadGenerator)o).getGeneratorId());
+        }
     }
 
     /** Simple job runner that immediately starts them and keeps restarting then they finish or die */
@@ -237,7 +451,7 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
 
         private String jobNameRegex = null;
 
-        private int jobConcurrency = 1;
+        private int desiredRunCount = 1;
 
         @Override
         public List<Job> getCandidateJobs() {
@@ -254,6 +468,7 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
             return CurrentTestMode.IDLE;
         }
 
+
         public String getJobNameRegex() {
             return jobNameRegex;
         }
@@ -262,18 +477,19 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
             this.jobNameRegex = jobNameRegex;
         }
 
-        public int getJobConcurrency() {
-            return jobConcurrency;
+        @Override
+        public int getDesiredRunCount() {
+            return desiredRunCount;
         }
 
-        public void setJobConcurrency(int jobConcurrency) {
-            this.jobConcurrency = jobConcurrency;
+        public void setDesiredRunCount(int desiredRunCount) {
+            this.desiredRunCount = desiredRunCount;
         }
 
-        public TrivialLoadGenerator(@CheckForNull String jobNameRegex, int jobConcurrency) {
+        public TrivialLoadGenerator(@CheckForNull String jobNameRegex, int desiredRunCount) {
             setJobNameRegex(jobNameRegex);
-            if (jobConcurrency < 0) {  // TODO Jelly form validation to reject this
-                this.jobConcurrency = 1;
+            if (desiredRunCount < 0) {  // TODO Jelly form validation to reject this
+                this.desiredRunCount = 1;
             }
         }
 
@@ -293,7 +509,7 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
     public static class BellsAndWhistlesLoadGenerator extends LoadGenerator {
         private String jobNameFilter = null;
 
-        private double desiredCount = 0.0;
+        private int desiredRunCount = 1;
 
         private long rampUpTimeMillis;
 
@@ -309,12 +525,13 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
             this.jobNameFilter = jobNameFilter;
         }
 
-        public double getDesiredCount() {
-            return desiredCount;
+        @Override
+        public int getDesiredRunCount() {
+            return desiredRunCount;
         }
 
-        public void setDesiredCount(double desiredCount) {
-            this.desiredCount = desiredCount;
+        public void setDesiredRunCount(int desiredRunCount) {
+            this.desiredRunCount = desiredRunCount;
         }
 
         /** Ramp up time for load*/
@@ -345,9 +562,9 @@ public class LoadGeneration extends AbstractDescribableImpl<LoadGeneration>  {
         }
 
         @DataBoundConstructor
-        public BellsAndWhistlesLoadGenerator(@CheckForNull String jobNameFilter, double desiredCount, long rampUpTimeMillis, long shutDownTimeoutMillis) {
+        public BellsAndWhistlesLoadGenerator(@CheckForNull String jobNameFilter, int desiredRunCount, long rampUpTimeMillis, long shutDownTimeoutMillis) {
             setJobNameFilter(jobNameFilter);
-            setDesiredCount(desiredCount);
+            setDesiredRunCount(desiredRunCount);
             setRampUpTimeMillis(rampUpTimeMillis);
             setShutDownTimeoutMillis(shutDownTimeoutMillis);
         }

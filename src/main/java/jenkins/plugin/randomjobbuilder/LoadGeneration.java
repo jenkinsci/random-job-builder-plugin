bfe46fc64bd2107aa7ca5bfad675a06f6eaa641f
Sam Van Oort <samvanoort@gmail.com>
2017-08-07 11:15:07 -0400

    Clean up import

--- src/main/java/jenkins/plugin/randomjobbuilder/LoadGeneration.java
+++ src/main/java/jenkins/plugin/randomjobbuilder/LoadGeneration.java
@@ -41,7 +41,6 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
